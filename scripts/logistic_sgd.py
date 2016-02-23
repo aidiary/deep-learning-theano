60b6d22079d48cd2de43ef27b1b3a2e170886941
sylvan5 <f2forest@gmail.com>
2016-02-23 23:38:27 +0900

    Meet the pep8 standards

--- scripts/logistic_sgd.py
+++ scripts/logistic_sgd.py
@@ -1,8 +1,6 @@
-#coding: utf-8
+# coding: utf-8
 import cPickle
 import gzip
-import os
-import sys
 import time
 
 import numpy as np
@@ -10,22 +8,27 @@ import numpy as np
 import theano
 import theano.tensor as T
 
+
 class LogisticRegression(object):
+
     def __init__(self, input, n_in, n_out):
         """ロジスティック回帰モデルの初期化
+
         input: ミニバッチ単位のデータ行列（n_samples, n_in）
         n_in : 入力の次元数
-        n_out: 出力の次元数"""
+        n_out: 出力の次元数
+        """
         # 重み行列を初期化
-        self.W = theano.shared(value=np.zeros((n_in, n_out), dtype=theano.config.floatX),
+        self.W = theano.shared(value=np.zeros((n_in, n_out),
+                                              dtype=theano.config.floatX),
                                name='W',
                                borrow=True)
 
         # バイアスベクトルを初期化
-        self.b = theano.shared(value=np.zeros((n_out,), dtype=theano.config.floatX),
+        self.b = theano.shared(value=np.zeros((n_out,),
+                                              dtype=theano.config.floatX),
                                name='b',
                                borrow=True)
-
 
         # 各サンプルが各クラスに分類される確率を計算するシンボル
         # 全データを行列化してまとめて計算している
@@ -41,12 +44,15 @@ class LogisticRegression(object):
 
     def negative_log_likelihood(self, y):
         """誤差関数である負の対数尤度を計算するシンボルを返す
-        yにはinputに対応する正解クラスを渡す"""
+
+        yにはinputに対応する正解クラスを渡す
+        """
         # 式通りに計算するとsumだがmeanの方がよい
         return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
 
     def errors(self, y):
         """分類の誤差率を計算するシンボルを返す
+
         yにはinputに対応する正解クラスを渡す"""
         if y.ndim != self.y_pred.ndim:
             raise TypeError('y should have the same shape as self.y_pred',
@@ -56,6 +62,7 @@ class LogisticRegression(object):
             return T.mean(T.neq(self.y_pred, y))
         else:
             raise NotImplementedError()
+
 
 def load_data(dataset):
     """データセットをロードしてGPUの共有変数に格納"""
@@ -67,8 +74,10 @@ def load_data(dataset):
         data_x, data_y = data_xy
 
         # 共有変数には必ずfloat型で格納
-        shared_x = theano.shared(np.asarray(data_x, dtype=theano.config.floatX), borrow=borrow)
-        shared_y = theano.shared(np.asarray(data_y, dtype=theano.config.floatX), borrow=borrow)
+        shared_x = theano.shared(
+            np.asarray(data_x, dtype=theano.config.floatX), borrow=borrow)
+        shared_y = theano.shared(
+            np.asarray(data_y, dtype=theano.config.floatX), borrow=borrow)
 
         # ラベルはint型なのでキャストして返す
         return shared_x, T.cast(shared_y, 'int32')
@@ -82,6 +91,7 @@ def load_data(dataset):
             (test_set_x, test_set_y)]
 
     return rval
+
 
 def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000, batch_size=600):
     # 学習データの準備
@@ -109,13 +119,7 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000, batch_size=600):
     # MNISTの手書き数字を分類するロジスティック回帰モデル
     # 入力は28ピクセルx28ピクセルの画像、出力は0から9のラベル
     # 入力はシンボルxを割り当てておいてあとで具体的なデータに置換する
-    classifier = LogisticRegression(input=x, n_in=28*28, n_out=10)
-
-    # シンボルのサイズを知りたいとき
-    # シンボルに具体的なデータを与えて評価しないと取得できない
-#     get_shape = theano.function([index], classifier.p_y_given_x.shape,
-#                         givens={x: train_set_x[index * batch_size: (index + 1) * batch_size]})
-#     print get_shape(0)
+    classifier = LogisticRegression(input=x, n_in=28 * 28, n_out=10)
 
     # 誤差（コスト）を計算 => 最小化したい
     cost = classifier.negative_log_likelihood(y)
@@ -178,37 +182,50 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000, batch_size=600):
         epoch = epoch + 1
         for minibatch_index in xrange(n_train_batches):
             # minibatch_index番目の訓練データのミニバッチを用いてパラメータ更新
-            minibatch_avg_cost = train_model(minibatch_index)
+            train_model(minibatch_index)
 
             # validation_frequency回の更新ごとにバリデーションセットによるモデル検証が入る
-            iter = (epoch - 1) * n_train_batches + minibatch_index
-            if (iter + 1) % validation_frequency == 0:
+            iteration = (epoch - 1) * n_train_batches + minibatch_index
+            if (iteration + 1) % validation_frequency == 0:
                 # バリデーションセットの平均エラー率を計算
-                validation_losses = [validate_model(i) for i in xrange(n_valid_batches)]
+                validation_losses = [
+                    validate_model(i) for i in xrange(n_valid_batches)]
                 this_validation_loss = np.mean(validation_losses)
-                print "epoch %i, minibatch %i/%i, validation error %f %%" % (epoch, minibatch_index + 1, n_train_batches, this_validation_loss * 100)
+                print "epoch %i, minibatch %i/%i, validation error %f %%" % \
+                    (epoch, minibatch_index + 1, n_train_batches,
+                     this_validation_loss * 100)
 
                 # エラー率が十分改善したならまだモデル改善の余地があるためpatienceを上げてより多くループを回せるようにする
                 if this_validation_loss < best_validation_loss:
-                    if this_validation_loss < best_validation_loss * improvement_threshold:
-                        patience = max(patience, iter * patience_increase)
-                        print "*** iter %d / patience %d" % (iter, patience)
+                    if this_validation_loss < \
+                            best_validation_loss * improvement_threshold:
+                        patience = max(patience, iteration * patience_increase)
+                        print "*** iteration %d / patience %d" % \
+                            (iteration, patience)
 
                     best_validation_loss = this_validation_loss
 
                     # テストセットを用いたエラー率も求めておく
-                    test_losses = [test_model(i) for i in xrange(n_test_batches)]
+                    test_losses = [test_model(i)
+                                   for i in xrange(n_test_batches)]
                     test_score = np.mean(test_losses)
-                    print "    epoch %i, minibatch %i/%i, test error of best model %f %%" % (epoch, minibatch_index + 1, n_train_batches, test_score * 100)
+                    print "    epoch %i, minibatch %i/%i, " \
+                        "test error of best model %f %%" % \
+                        (epoch,
+                         minibatch_index + 1, n_train_batches,
+                         test_score * 100)
 
             # patienceを超えたらループを終了
-            if patience <= iter:
+            if patience <= iteration:
                 done_looping = True
                 break
 
     end_time = time.clock()
-    print "Optimization complete with best validation score of %f %%, with test performance %f %%" % (best_validation_loss * 100, test_score * 100)
-    print "The code run for %d epochs, with %f epochs/sec" % (epoch, 1.0 * epoch / (end_time - start_time))
+    print "Optimization complete with best validation score of %f %%, " \
+        "with test performance %f %%" % \
+        (best_validation_loss * 100, test_score * 100)
+    print "The code run for %d epochs, with %f epochs/sec" % \
+        (epoch, 1.0 * epoch / (end_time - start_time))
 
 if __name__ == "__main__":
     sgd_optimization_mnist()
